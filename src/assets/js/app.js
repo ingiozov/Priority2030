9e1d0bb60812ea9c3c3ab5b61b05ceeb698794e4
ingiozov <ingiozov@noreply.github.com>
2021-04-19 10:49:53 +0300

    carousel added need to fix controls

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -16,5 +16,21 @@ require('foundation-sites')
 $(document).foundation()
 
 $(document).ready(function () {
-  $('.owl-carousel').owlCarousel()
+  $('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 29,
+    nav: true,
+    dots: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 2,
+      },
+      1000: {
+        items: 3,
+      },
+    },
+  })
 })
