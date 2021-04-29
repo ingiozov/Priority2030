98efef7f4fc398fb7e2075701148c61767f6da83
ingiozov <ingiozov@noreply.github.com>
2021-04-29 22:56:35 +0300

    sticky nav added

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -51,3 +51,29 @@ $(function () {
     $(this).find('ul').stop(true, true).slideToggle()
   })
 })
+//////////////////////////
+// nav
+
+// const header = document.querySelector('.intro')
+// const headerGrant = document.querySelector('.grant-intro-container')
+// const nav = document.querySelector('.nav')
+
+// const navHeight = nav.getBoundingClientRect().height
+
+// const stickyNav = function (entries) {
+//   const [entry] = entries
+
+//   if (!entry.isIntersecting) nav.classList.add('sticky2')
+//   else nav.classList.remove('sticky2')
+// }
+
+// const headerObserver = new IntersectionObserver(stickyNav, {
+//   root: null,
+//   threshold: 0,
+//   rootMargin: `-${navHeight}px`,
+// })
+
+// headerObserver.observe(header)
+// headerObserver.observe(headerGrant)
+
+////////////////////////
