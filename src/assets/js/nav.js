8b9c3aad499f03bbcc723178a83281c7c51a1364
ingiozov <ingiozov@noreply.github.com>
2021-05-06 19:53:21 +0300

    added new news, firststep in progress

---
+++ src/assets/js/nav.js
@@ -0,0 +1,19 @@
+const header = document.querySelector('.intro')
+const nav = document.querySelector('.nav')
+
+const navHeight = nav.getBoundingClientRect().height
+
+const stickyNav = function (entries) {
+  const [entry] = entries
+
+  if (!entry.isIntersecting) nav.classList.add('sticky2')
+  else nav.classList.remove('sticky2')
+}
+
+const headerObserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${navHeight}px`,
+})
+
+headerObserver.observe(header)
